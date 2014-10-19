a382661f2a82b5a7878e455e2ebb8cf26c19eca6
Haowu Ge <ghw@7axu.com>
2014-10-20 05:14:15 +0800

    first commit

---
+++ trunk/feeds/routing.tmp/.host.mk
@@ -0,0 +1,9 @@
+HOST_OS:=FreeBSD
+HOST_ARCH:=amd64
+GNU_HOST_NAME:=x86_64-unknown-freebsd10.0
+TAR:=/usr/bin/tar
+FIND:=/usr/bin/find
+BASH:=/bin/bash
+FIND_L=/usr/bin/find -L $(1)
+XARGS:=xargs
+PATCH:=/usr/local/bin/gpatch
